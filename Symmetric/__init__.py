eb621fc4b71a82b915877fb750c043c429f27156
S L <1006682997@qq.com>
2020-02-24 20:57:15 +0100

    point-mass lens model built

---
+++ Symmetric/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 16:35:39 2020
+
+@author: lshuns
+
+Module dealing with symmetric lens models
+"""
+
+# point-mass lens model
+from Symmetric.PointMass import PointMassFunc, KummerComplexFunc
+
+
+
